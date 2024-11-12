046f08e3d057572bd2f629b43d1b57cd47ced7e6
Isabela-sanchez-mol <isabela.sanchez_mol@uao.edu.co>
2024-11-12 13:00:36 -0500

    tercer commit

--- PDI-main/src/App.js
+++ PDI-main/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import UploadAndClassify from './components/UploadAndClassify';
-import './styles/App.css';  // Importa tus estilos si tienes
+import './styles/App.css';  // Asegúrate de tener la importación del CSS
+import UploadAndClassify from './components/UploadAndClassify'; // Importa tu componente
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Conexión Cultural: <br></br>Historias a través del Arte</h1>
+      <h1>Conexión Cultural: <br />Historias a través del Arte</h1>
       <p>Haz tu dibujo, súbelo y descubre su historia</p>
-      <UploadAndClassify />
+      
+      <div className="bubbles-container">
+        <div className="bubble-left">
+          <h3>Buziraco</h3>
+        </div>
+        
+        <div className="upload-container">
+          <UploadAndClassify/>
+        </div>
+
+        <div className="bubble-right">
+          <h3>La Candileja</h3>
+        </div>
+      </div>
     </div>
   );
 };
--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import './styles/App.css';  // Importa tus estilos si tienes
 const App = () => {
   return (
     <div className="App">
-      <h1>Conexión Cultural: <br></br>Historias a través del Arte</h1>
+      <h1>Conexión Cultural: <br />Historias a través del Arte</h1>
       <p>Haz tu dibujo, súbelo y descubre su historia</p>
+      <div className="bubble-left">
+        <img className="bubble-image" src="../src/assets/candileja_ej.jpg" alt="La Candileja"/>
+        <h3>La Candileja</h3>
+      </div>
+      <div className="bubble-right">
+      <img className="bubble-image" src="/src/assets/buziraco_ej.jpg" alt="Buziraco" />
+      <h3>Buziraco</h3>
+      </div>
       <UploadAndClassify />
     </div>
   );
